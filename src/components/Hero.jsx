aac4bc18a3510a50866903e65c96438923162480
abyansetya <abyansetyaa@gmail.com>
2025-09-21 23:56:49 +0700

    update: update UI

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,12 +3,15 @@ import profilepict from "../assets/profilepict.svg";
 import cv from "../assets/CV_AbyanSetyaneva_Updated.pdf";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 import { motion } from "framer-motion";
+import ShinyText from "@/lib/ShinyText";
+// impor dari emoji-mart versi baru
+import data from "@emoji-mart/data";
+import { init } from "emoji-mart";
+import DarkVeil from "@/lib/DarkVeil";
+
+init({ data });
 
 export default function Hero() {
-  function readme() {
-    window.open(cv);
-  }
-
   const [text] = useTypewriter({
     words: ["ull Stack Developer"],
     loop: {},
@@ -16,42 +19,36 @@ export default function Hero() {
     deleteSpeed: 50,
   });
 
+  function readme() {
+    window.open(cv);
+  }
+
   return (
     <div className="flex justify-center sm:gap-[5rem] gap-[1rem] py-20 h-[100vh] px-16 w-full md:flex-row flex-col z-1">
-      <motion.img
-        initial={{ scale: 0, opacity: 0 }}
-        animate={{ scale: 1, opacity: 1 }}
-        transition={{
-          type: "spring",
-          stiffness: 100,
-          damping: 20,
-        }}
-        src={profilepict}
-        alt=""
-        className="flex h-[300px] w-[300px] sm:h-[400px] sm:w-[400px]  self-center  "
-        draggable="false"
-      />
-      <div className="self-center text-center font-poppins">
-        <p className="font-[600] text-center">Hello, I'm</p>
-        <h1 className="font-[600] text-[2rem] sm:text-[3rem] text-center">
-          Abyan Setya
-        </h1>
-        <span className="font-[600] text-2xl mb-1">F{text}</span>
-        <span>
-          <Cursor />
-        </span>
-        <div className="flex items-center justify-center gap-1 text-[#7d7d7d] font-poppins mt-5">
-          <button
-            className="hover:text-black cursor-pointer hover:underline"
-            onClick={readme}
-          >
-            Readme{" "}
-          </button>
-          <p className="text-black font-bold"> or</p>
-          <button className="hover:text-black cursor-pointer hover:underline">
-            <a href="#Projects">View Projects</a>
-          </button>
+      <div className="self-center text-center font-poppins font-[500] flex items-center gap-2 flex-col">
+        <p className="text-4xl">
+          Zup! <em-emoji native="👋" set="apple"></em-emoji> I'm{" "}
+          <span className="bg-black text-white px-4 rounded-full">
+            Abyan Setya.
+          </span>
+        </p>
+        <div className="flex">
+          <ShinyText
+            text="a Full-Stack Web Developer "
+            speed={3}
+            className="text-4xl"
+          />
+          <p className="text-4xl">
+            <em-emoji native="👨‍💻" set="apple"></em-emoji>
+          </p>
         </div>
+        <p className="text-4xl">
+          Turning Ideas{" "}
+          <span>
+            <em-emoji native="💡" set="apple"></em-emoji>
+          </span>{" "}
+          Into Powerful Web Experiences.
+        </p>
       </div>
     </div>
   );
